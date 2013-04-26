5809a69b1e290c1decc613d9ae51f3f0f28f93bd
Jörg Liebig <joliebig@fim.uni-passau.de>
2013-04-26 10:17:16 +0200

    reformat code

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/CheckDataflow.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/CheckDataflow.scala
@@ -7,23 +7,23 @@ import de.fosd.typechef.typesystem.{CDeclUse, CTypeSystemFrontend}
 object CheckDataflow extends IntraCFG with CFGHelper with Liveness {
 
     def checkDataflow(tunit: TranslationUnit, fm: FeatureModel = FeatureExprFactory.default.featureModelFactory.empty) {
-      val fdefs = filterAllASTElems[FunctionDef](tunit)
-      val ts = new CTypeSystemFrontend(tunit, fm) with CDeclUse
-      val udm = ts.getUseDeclMap
-      fdefs.map(intraDataflowAnalysis(_, fm, udm))
+        val fdefs = filterAllASTElems[FunctionDef](tunit)
+        val ts = new CTypeSystemFrontend(tunit, fm) with CDeclUse
+        val udm = ts.getUseDeclMap
+        fdefs.map(intraDataflowAnalysis(_, fm, udm))
     }
 
     private def intraDataflowAnalysis(f: FunctionDef, fm: FeatureModel, udm: UseDeclMap) {
-      if (f.stmt.innerStatements.isEmpty) return
+        if (f.stmt.innerStatements.isEmpty) return
 
-      val env = CASTEnv.createASTEnv(f)
-      setEnv(env)
-      val ss = getAllSucc(f.stmt.innerStatements.head.entry, FeatureExprFactory.empty, env)
-      setFm(fm)
-      setUseDeclMap(udm)
+        val env = CASTEnv.createASTEnv(f)
+        setEnv(env)
+        val ss = getAllSucc(f.stmt.innerStatements.head.entry, FeatureExprFactory.empty, env)
+        setFm(fm)
+        setUseDeclMap(udm)
 
-      val nss = ss.map(_._1).filterNot(x => x.isInstanceOf[FunctionDef])
-      for (s <- nss) in(s)
+        val nss = ss.map(_._1).filterNot(x => x.isInstanceOf[FunctionDef])
+        for (s <- nss) in(s)
     }
 
 }
